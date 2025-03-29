27e3adb13b176a72c8c507b57accb04ccb5e54a5
Jaden Taylor <jtjadent@gmail.com>
2025-03-29 16:42:33 -0600

    testing out new changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,42 @@
-import './App.css'
-import BookList from './BookList'
+import './App.css';
+import { CartProvider } from './context/CartContext';
+import CartPage from './pages/CartPage';
+import DonatePage from './pages/PurchasePage';
+import ProjectsPage from './pages/BooksPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
-      <BookList/>
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<ProjectsPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route
+              path="/donate/:projectName/:projectId"
+              element={<DonatePage />}
+            />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </Router>
+      </CartProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// import './App.css'
+// import BookList from './BookList'
+
+// function App() {
+
+//   return (
+//     <>
+//       <BookList/>
+//     </>
+//   )
+// }
+
+// export default App
