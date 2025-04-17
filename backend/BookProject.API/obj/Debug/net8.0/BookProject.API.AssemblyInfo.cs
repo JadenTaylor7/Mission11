11bd3edf6e343d3774b542e325403e4ad73375c1
Jaden Taylor <jtjadent@gmail.com>
2025-04-17 11:17:56 -0600

    random stuff

--- backend/BookProject.API/obj/Debug/net8.0/BookProject.API.AssemblyInfo.cs
+++ backend/BookProject.API/obj/Debug/net8.0/BookProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5df757bfbb80eb3255e8cf47b8bb28d8fd9c294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f966e6bd2e86240650881d4104afbef3399bbb12")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
